0fdef39473d26d5ed5b181356b94dfaab5a2d3d7
dannoonan <danjnoonan@hotmail.com>
2018-11-08 17:37:45 +0000

    Added view fragments for admin functionality

---
+++ Android/app/src/main/java/com/macmanus/jamie/bikerentalapp/view/AddBikeFragment.java
@@ -0,0 +1,30 @@
+package com.macmanus.jamie.bikerentalapp.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.macmanus.jamie.bikerentalapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddBikeFragment extends Fragment {
+
+
+    public AddBikeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_bike, container, false);
+    }
+
+}
