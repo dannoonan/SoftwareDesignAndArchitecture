7417a6bf34ae2778d0828530a9cfa3e8a1c24446
JamieMac96 <jamiemac96@hotmail.com>
2018-11-13 14:02:18 +0000

    Changed BASE_URL in RetrofitInstance.java to reflect our API

--- Android/app/src/main/java/com/macmanus/jamie/bikerentalapp/web/RetrofitInstance.java
+++ Android/app/src/main/java/com/macmanus/jamie/bikerentalapp/web/RetrofitInstance.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "https://api.github.com/";
+    private static final String BASE_URL = "http://34.254.254.125:1234";
 
     /**
      * Create an instance of Retrofit object
