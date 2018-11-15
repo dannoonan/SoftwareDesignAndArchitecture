4b7ff84ce0bcb69dd4c94b1af3a8039d3f7686ac
FrankSun96 <franksun1996@gmail.com>
2018-11-15 20:54:34 +0000

    finish rent and return

--- springBootDemo/src/main/java/ie/demo/controller/OrderController.java
+++ springBootDemo/src/main/java/ie/demo/controller/OrderController.java
@@ -1,8 +1,10 @@
 package ie.demo.controller;
 
+import ie.demo.service.BikeService;
 import ie.demo.service.OrderService;
 import ie.response.MsgResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
+	
 
 	@Autowired
 	private OrderService orderService;
@@ -30,4 +33,41 @@ public class OrderController {
 			return MsgResponse.fail(result).add("error", "Failed to place order.");
 		}
 	}
+	
+	@RequestMapping(value= "/rent/{id}", method=RequestMethod.POST, produces="application/json;charset=UTF-8")
+	public MsgResponse rentBike(@PathVariable int id, @RequestParam(value = "userId")int userId) {
+		int result = orderService.bikeRent(id, userId);
+		if(result == -1) {
+			return MsgResponse.fail(404).add("error", "balance is not enough");
+		} else if(result == 0){
+			return MsgResponse.fail(404).add("error", "Failed to place order.");
+		} else if(result == -2) {
+			return MsgResponse.fail(result).add("error", "Bike or User does not exist.");
+		} else {
+			return MsgResponse.success().add("orderId", result);
+		}
+	}
+	
+	@RequestMapping(value= "/money/{minutes}", method=RequestMethod.GET, produces="application/json;charset=UTF-8")
+	public MsgResponse calculateDeductions(@PathVariable float minutes) {
+		float amountPaid = orderService.calculateDeductions(minutes);
+		return MsgResponse.success().add("amountPaid", amountPaid);
+	}
+	
+	@RequestMapping(value= "/return", method=RequestMethod.POST, produces="application/json;charset=UTF-8")
+	public MsgResponse returnBike(@RequestParam(value = "orderId") int orderId, 
+			@RequestParam(value = "latitude")float latitude, 
+			@RequestParam(value = "longitude")float longitude,
+			@RequestParam(value = "amountPaid") float amountPaid,
+			@RequestParam(value = "studentCardId") int studentCardId,
+			@RequestParam(value = "nodeId") int nodeId) {
+		int result = orderService.bikeReturn(orderId, latitude, longitude, amountPaid, studentCardId, nodeId);
+		if(result == -1) {
+			return MsgResponse.fail(404).add("error", "balance is not enough");
+		} else {
+			return MsgResponse.success();
+		}
+	}
+	
+	
 }
