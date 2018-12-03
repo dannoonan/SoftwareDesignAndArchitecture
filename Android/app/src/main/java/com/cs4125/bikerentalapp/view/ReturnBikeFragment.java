0b91d9806a83c2a9ebcd827c87fe305096ced79c
JamieMac96 <jamiemac96@hotmail.com>
2018-12-03 00:12:56 +0000

    Changed fields to private in returnbikefragment

--- Android/app/src/main/java/com/cs4125/bikerentalapp/view/ReturnBikeFragment.java
+++ Android/app/src/main/java/com/cs4125/bikerentalapp/view/ReturnBikeFragment.java
@@ -30,12 +30,12 @@ import java.io.IOException;
 
 public class ReturnBikeFragment extends Fragment {
 
-    SurfaceView surfaceView;
-    TextView txtBarcodeValue;
+    private SurfaceView surfaceView;
+    private TextView txtBarcodeValue;
     private CameraSource cameraSource;
     private static final int REQUEST_CAMERA_PERMISSION = 201;
-    TextView txtView;
-    String intentData = "";
+    private TextView txtView;
+    private String intentData = "";
 
 
     @Nullable
