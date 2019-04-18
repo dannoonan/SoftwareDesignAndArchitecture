05701a3016bd7f7f7a6baede84b6de8a5e98e8f1
RoryEgan <roryegan1997@gmail.com>
2019-04-18 16:22:27 +0100

    Forgot @Override.

--- springBootDemo/src/main/java/ie/util/NormalStrategy.java
+++ springBootDemo/src/main/java/ie/util/NormalStrategy.java
@@ -2,6 +2,7 @@ package ie.util;
 
 public class NormalStrategy implements BillingStrategy {
 
+    @Override
     public float getActPrice(float basePrice) {
         return basePrice;
     }
--- springBootDemo/src/main/java/ie/util/PremiumStrategy.java
+++ springBootDemo/src/main/java/ie/util/PremiumStrategy.java
@@ -2,6 +2,7 @@ package ie.util;
 
 public class PremiumStrategy implements BillingStrategy {
 
+    @Override
     public float getActPrice(float basePrice) {
         return basePrice / 2;
     }
