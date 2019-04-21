d23bf5df2ec714a314670d2b841fb7590835da2a
RoryEgan <roryegan1997@gmail.com>
2019-04-21 13:02:58 +0100

    Changed to git pull instead of rsync.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 ssh -oStrictHostKeyChecking=no -i "cs4227_key.pem" ubuntu@ec2-54-208-196-22.compute-1.amazonaws.com 
-cd SoftwareDesignAndArchitecture && 
-git pull && 
-cd springBootDemo && 
-mvn clean install -DskipTests && 
+cd SoftwareDesignAndArchitecture
+git pull
+cd springBootDemo
+mvn clean install -DskipTests
 nohup mvn spring-boot:run -DskipTests &
