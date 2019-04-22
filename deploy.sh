3679ca15a414efe189e433d06f3abb2bc6e71357
RoryEgan <roryegan1997@gmail.com>
2019-04-22 13:46:52 +0100

    Final push to finish setting up CI automatic deployment to EC2 server.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-#scp -i "cs4227_key.pem" -r springBootDemo/. ubuntu@ec2-54-208-196-22.compute-1.amazonaws.com:~/SoftwareDesignAndArchitecture
-#ssh -i "cs4227_key.pem" ubuntu@ec2-54-208-196-22.compute-1.amazonaws.com 'cd SoftwareDesignAndArchitecture/springBootDemo | mvn clean install | nohup spring-boot:run &' 
-cd SoftwareDesignAndArchitecture/springBootDemo && mvn clean install -DskipTests && nohup mvn spring-boot:run -DskipTests &
+#!/bin/bash
+
+ssh -oStrictHostKeyChecking=no -tt -i "cs4227_key.pem" ubuntu@ec2-54-208-196-22.compute-1.amazonaws.com "cd SoftwareDesignAndArchitecture; git pull; cd springBootDemo; mvn clean install -DskipTests; nohup mvn spring-boot:run -DskipTests &"
