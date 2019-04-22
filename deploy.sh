2733bdbb4071d611bd6603dac29662307cd07a31
RoryEgan <roryegan1997@gmail.com>
2019-04-22 13:34:23 +0100

    Attempting to fix error code 127.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-ssh -oStrictHostKeyChecking=no -tt -i "cs4227_key.pem" ubuntu@ec2-54-208-196-22.compute-1.amazonaws.com &&
-cd SoftwareDesignAndArchitecture &&
-git pull &&
-cd springBootDemo &&
-mvn clean install -DskipTests &&
-nohup mvn spring-boot:run -DskipTests &
+ssh -oStrictHostKeyChecking=no -tt -i "cs4227_key.pem" ubuntu@ec2-54-208-196-22.compute-1.amazonaws.com "cd SoftwareDesignAndArchitecture && git pull && cd springBootDemo && mvn clean install -DskipTests && nohup mvn spring-boot:run -DskipTests &"
